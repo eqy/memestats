fb1c51b22c68fbded2dd3d0480aec868b94e6dc2
Eddie Yan <Loser777@gmail.com>
2015-08-21 10:48:53 -0700

    fix timeout isue

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 while true;
 do
-	time python3 memestats.py
-	cp desktop /var/www/html/
-	cp battlestation /var/www/html/
+    time python3 memestats.py & sleep 300; kill $!
+    cp desktop /var/www/html/
+    cp battlestation /var/www/html/
     cp thinkpad /var/www/html/
 	cp koptek.txt /var/www/html/
     cp name /var/www/html/
